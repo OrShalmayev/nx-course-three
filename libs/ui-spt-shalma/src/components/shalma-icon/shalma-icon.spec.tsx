171b29f0b2b8a000a57aa87d9cff8bedb9acdd80
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2023-05-14 13:12:19 +0300

    [src] added cmp to ui-spt-shalma

---
+++ libs/ui-spt-shalma/src/components/shalma-icon/shalma-icon.spec.tsx
@@ -0,0 +1,36 @@
+import { newSpecPage } from '@stencil/core/testing';
+import { ShalmaIcon } from './shalma-icon';
+
+describe('shalma-icon', () => {
+  it('renders', async () => {
+    const { root } = await newSpecPage({
+      components: [ShalmaIcon],
+      html: '<shalma-icon></shalma-icon>',
+    });
+    expect(root).toEqualHtml(`
+      <shalma-icon>
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm
+          </div>
+        </mock:shadow-root>
+      </shalma-icon>
+    `);
+  });
+
+  it('renders with values', async () => {
+    const { root } = await newSpecPage({
+      components: [ShalmaIcon],
+      html: `<shalma-icon first="Stencil" last="'Don't call me a framework' JS"></shalma-icon>`,
+    });
+    expect(root).toEqualHtml(`
+      <shalma-icon first="Stencil" last="'Don't call me a framework' JS">
+        <mock:shadow-root>
+          <div>
+            Hello, World! I'm Stencil 'Don't call me a framework' JS
+          </div>
+        </mock:shadow-root>
+      </shalma-icon>
+    `);
+  });
+});
---
+++ libs/ui-spt-shalma/src/components/shalma-icon/shalma-icon.tsx
@@ -0,0 +1,24 @@
+import {Component, h, Prop} from '@stencil/core';
+import {allIcons, SpotIcons, SpotIconsName} from "@nx-course-three/svg-to-ts";
+
+
+@Component({
+  tag: 'spot-icon',
+  styleUrl: 'spot-icon.scss',
+  scoped: true
+})
+export class SpotIcon {
+  spotIconsName: typeof SpotIconsName = SpotIconsName;
+  allIcons: SpotIcons[] = allIcons
+
+  @Prop() iconName!: string;
+
+  private getIcon(): string {
+    //TODO handle when icon name is not defined
+    return allIcons.find(icon => icon.name === this.iconName).data;
+  }
+
+  render() {
+    return <div innerHTML={this.getIcon()}></div>;
+  }
+}
